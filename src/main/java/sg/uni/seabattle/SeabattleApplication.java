3e0ea50e270a5c1c823e723815961c13148f7d04
Saveli Gulas <saveli.gulas@gmail.com>
2025-03-06 22:00:26 +0100

    Initial commit

---
+++ src/main/java/sg/uni/seabattle/SeabattleApplication.java
@@ -0,0 +1,13 @@
+package sg.uni.seabattle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeabattleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeabattleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sg/uni/seabattle/SeabattleApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.uni.seabattle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeabattleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
